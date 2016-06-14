ca85eae0e4bacd3e9dd959d51b2b26467ec0986c
Levi Hopkins and Noah Ramey <noah@>
2016-06-14 14:11:58 -0700

    add specs to javascript and html

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
 // BUSINESS LOGIC //
-function Ticket(time, age) {
+var darkKnight = "The Dark Knight"
+var toyStory = "Toy Story"
+var pulpFiction = "Pulp Fiction"
+var youthPrice = "$7.50"
+var seniorPrice = "$7.00"
+var adultPrice = "$9.50"
+
+function Ticket(time, age, movie, number) {
   this.time = time;
   this.age = age;
+  this.movie = movie;
+  this.number = number;
 }
 
+var total = function(price, number) {
+  return price * number;
+}
 
+Ticket.prototype.ticketPrice = function() {
+  return "Your " + this.number + " " + this.age + " ticket(s) for the " + this.time + " showing of " + this.movie + " will come to a total of $"
+}
 
 
 
@@ -18,14 +33,23 @@ $(function() {
 
     var darkKnightShowtime = $("#dark-knight-showtime").val();
     var darkKnightAge = $("#dark-knight-age").val();
+    var darkKnightNumber = parseInt($("#dark-knight-number").val());
+    var price;
+    if (darkKnightAge === "Youth") {
+      var price = 7.50;
+    } else if (darkKnightAge === "Senior") {
+      var price = 7.00;
+    } else {
+      var price = 9.50;
+    }
+    price = total(price, darkKnightNumber);
+    var darkKnightTicket = new Ticket(darkKnightShowtime, darkKnightAge, darkKnight, darkKnightNumber);
 
-    var darkKnightTicket = new Ticket(darkKnightShowtime, darkKnightAge);
 
     $("#body-fade").fadeOut();
     $("#image-one").show();
-    $(".movie").text("Dark Knight")
-    $(".age").text(darkKnightTicket.age);
-    $(".time").text(darkKnightTicket.time);
+    $(".order-msg").append(darkKnightTicket.ticketPrice());
+    $(".order-msg").append(price);
     $(".order").fadeIn();
 
   });
@@ -35,14 +59,23 @@ $(function() {
 
     var toyStoryShowtime = $("#toy-story-showtime").val();
     var toyStoryAge = $("#toy-story-age").val();
+    var toyStoryNumber = parseInt($("#toy-story-number").val());
+    var price;
+    if (toyStoryAge === "Youth") {
+      var price = 7.50;
+    } else if (toyStoryAge === "Senior") {
+      var price = 7.00;
+    } else {
+      var price = 9.50;
+    }
+    price = total(price, toyStoryNumber);
 
-    var toyStoryTicket = new Ticket(toyStoryShowtime, toyStoryAge);
+    var toyStoryTicket = new Ticket(toyStoryShowtime, toyStoryAge, toyStory, toyStoryNumber);
 
     $("#body-fade").fadeOut();
     $("#image-two").show();
-    $(".movie").text("Toy Story")
-    $(".age").text(toyStoryTicket.age);
-    $(".time").text(toyStoryTicket.time);
+    $(".order-msg").text(toyStoryTicket.ticketPrice());
+    $(".order-msg").append(price);
     $(".order").fadeIn();
   });
 
@@ -51,18 +84,27 @@ $(function() {
 
     var pulpFictionShowtime = $("#pulp-fiction-showtime").val();
     var pulpFictionAge = $("#pulp-fiction-age").val();
+    var pulpFictionNumber = parseInt($("#pulp-fiction-number").val());
+    var price;
+    if (pulpFictionAge === "Youth") {
+      var price = 7.50;
+    } else if (pulpFictionAge === "Senior") {
+      var price = 7.00;
+    } else {
+      var price = 9.50;
+    }
+    price = total(price, pulpFictionNumber);
 
-    if (pulpFictionAge = "Youth") {
+    if (pulpFictionAge === "Youth") {
       $("#body-fade").fadeOut();
       $("#not-old-enough").fadeIn();
     }
     else {
-      var pulpFictionTicket = new Ticket(pulpFictionShowtime, pulpFictionAge);
+      var pulpFictionTicket = new Ticket(pulpFictionShowtime, pulpFictionAge, pulpFiction, pulpFictionNumber);
       $("#body-fade").fadeOut();
       $("#image-three").show();
-      $(".movie").text("Pulp Fiction")
-      $(".age").text(pulpFictionTicket.age);
-      $(".time").text(pulpFictionTicket.time);
+      $(".order-msg").text(pulpFictionTicket.ticketPrice());
+      $(".order-msg").append(price);
       $(".order").fadeIn();
     }
   });
